c7c692746e3f4ddbafe427e10f93ba656d2637c0
giladk123 <83569702+giladk123@users.noreply.github.com>
2022-04-05 22:01:34 +0300

    Add files via upload

--- aws_deploy_ec2_spot.sh
+++ aws_deploy_ec2_spot.sh
@@ -10,5 +10,5 @@ AWS_SECRET=$5
 export AWS_ACCESS_KEY_ID=AKIA3WHUXPLQL4E62FP4 && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
 echo "Request your spot Instance from aws on $DATE" && \
-aws ec2 request-spot-instances --spot-price $SPOT_PRICE --instance-count $INST_COUNT --type $TYPE --launch-specification $JSON_FILE
+aws ec2 request-spot-instances --spot-price "$SPOT_PRICE" --instance-count $INST_COUNT --type "$TYPE" --launch-specification $JSON_FILE
 
