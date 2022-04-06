0e407278e359d905e9fa97578fe9c10698e1ea1a
giladk123 <83569702+giladk123@users.noreply.github.com>
2022-04-06 10:58:26 +0300

    add GMT time

--- aws_deploy_ec2_spot.sh
+++ aws_deploy_ec2_spot.sh
@@ -11,5 +11,5 @@ AWS_REGION=$6
 export AWS_ACCESS_KEY_ID=AKIA3WHUXPLQL4E62FP4 && \
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
 export AWS_DEFAULT_REGION=$AWS_REGION
-echo "Request your spot Instance from aws on $DATE" && \
+echo "Request your spot Instance from aws on $DATE GMT" && \
 aws ec2 request-spot-instances --spot-price "$SPOT_PRICE" --instance-count $INST_COUNT --type "$TYPE" --launch-specification $JSON_FILE
